77b5eb0fc8114daba698476e61f6a01ca8943122
Your Name <github email address>
2019-03-27 17:03:36 -0400

    some changes I dont know

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player'
 import '../App.css'
 
-const songImg = {
-  borderRadius: '1em',
-  height: '70px',
-  width: '70px',
-  paddingLeft: '5px',
-  paddingTop: '5px',
-  paddingBottom: '5px',
-  float: 'left',
-}
-
-const audioPlayer = {
-  height: '30px',
-  width: '200px'
-}
-
-const artistAndTitle = {
-  display: 'inline',
-}
-
-const artist = {
-  marginTop: '2px',
-  fontSize: '120%'
-}
-
-const title = {
-  fontSize: '90%'
-}
+// const songImg = {
+//   borderRadius: '1em',
+//   height: '70px',
+//   width: '70px',
+//   paddingLeft: '5px',
+//   paddingTop: '5px',
+//   paddingBottom: '5px',
+//   float: 'left',
+// }
+//
+// const audioPlayer = {
+//   height: '30px',
+//   width: '200px'
+// }
+//
+// const artistAndTitle = {
+//   display: 'inline',
+// }
+//
+// const artist = {
+//   marginTop: '2px',
+//   fontSize: '120%'
+// }
+//
+// const title = {
+//   fontSize: '90%'
+// }
 //***** STYLING *****//
 
 class Playlists extends Component {
